d28ba840701a9667356f935175aa7fa37f80c14b
anna.webb <anna.m.webb110@gmail.com>
2024-11-24 16:05:15 -0600

    after .venv setup

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,98 +1,161 @@
-############################
-# Imports
-############################
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
 
-from collections import deque
+# From shiny, import just reactive and render
+from shiny import reactive, render
 
+# From shiny.express, import just ui
+from shiny.express import ui
 
-############################
-# Create a deque
-############################
+# Imports from Python Standard Library to simulate live data
+import random
+from datetime import datetime
 
-# create a deque by calling the deque() constructor and passing in a list of values.
+# --------------------------------------------
+# Import icons as you like
+# --------------------------------------------
 
-# Create an empty deque
-empty_deque = deque()
-print(empty_deque) 
+from faicons import icon_svg
 
-# Create a deque by passing in a list with values
-temp_deque_F = deque( [56, 58, 47, 54, 55] )
-print(temp_deque_F)  
+# --------------------------------------------
+# FOR LOCAL DEVELOPMENT
+# --------------------------------------------
+# Add all packages not in the Std Library
+# to requirements.txt:
+#
+# faicons
+# shiny
+# shinylive
+# 
+# And install them into an active project virtual environment (usually in .venv)
+# --------------------------------------------
 
-# Create a deque by passing in a list variable
-temp_list_C = [5, 6, 8, 4, 3, 2]
-temp_deque_C = deque( temp_list_C )
-print(temp_deque_C )
+# --------------------------------------------
+# SET UP THE REACIVE CONTENT
+# --------------------------------------------
 
+# --------------------------------------------
+# PLANNING: We want to get a fake temperature and 
+# Time stamp every N seconds. 
+# For now, we'll avoid storage and just 
+# Try to get the fake live data working and sketch our app. 
+# We can do all that with one reactive calc.
+# Use constants for update interval so it's easy to modify.
+# ---------------------------------------------------------
 
-############################
-# Append Elements
-############################
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
 
-# append() method is for adding new data points to the end 
-# appendleft() method is used to append the front but isn't commonly used with live data
-temp_deque_F.append(60)
-temp_deque_F.append(62)
-temp_deque_F.append(64)
-temp_deque_F.append(61)
+UPDATE_INTERVAL_SECS: int = 1
 
-# check results
-print(temp_deque_F)
-
-
-############################
-# Remove Elements
-############################
-
-# use the pop() method to remove the end of the deque right to left
-# use popleft() method to remove from left to right 
-temp_deque_F.pop()  
-temp_deque_F.popleft()  
-
-# check results
-print(temp_deque_F)
-
-
-############################
-# Get Length of List (Count)
-############################
-
-#use python's built in len() function
-len(temp_deque_F)  
-
-
-############################
-# Clear the Deque
-############################
-
-#call the clear() method on the deque.
-temp_deque_F.clear()  
+# --------------------------------------------
+# Initialize a REACTIVE CALC that our display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+# It returns everything needed to display the data.
+# Very easy to expand or modify.
+# (I originally looked at REACTIVE POLL, but this seems to work better.)
+# --------------------------------------------
 
 
+@reactive.calc()
+def reactive_calc_combined():
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
 
-############################
-# Limit Deque Size
-############################
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -16), 1)
 
-# Initialize deque with a max length of 3 to store last 3 stock prices
-msft_prices = deque(maxlen=3)
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    # 
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
 
-# Clear the deque (we might call this at the start of a new day)
-msft_prices.clear()
+    # Return everything we need
+    return latest_dictionary_entry
 
-# Simulate updating the stock price with new values
-msft_prices.append(310.35)
-print("MSFT stock prices:", list(msft_prices))
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
 
-msft_prices.append(312.31)
-print("MSFT stock prices:", list(msft_prices))
+# Call the ui.page_opts() function
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
 
-msft_prices.append(315.25)
-print("MSFT stock prices:", list(msft_prices))
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
 
-msft_prices.append(317.41)
-print("MSFT stock prices:", list(msft_prices))
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
+
+with ui.sidebar(open="open"):
+
+    ui.h2("Antarctic Explorer", class_="text-center")
+
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
+
+    ui.hr()
+
+    ui.h6("Links:")
+
+    ui.a(
+        "GitHub Source",
+        href="https://github.com/denisecase/cintel-05-cintel-basic",
+        target="_blank",
+    )
+
+    ui.a(
+        "GitHub App",
+        href="https://denisecase.github.io/cintel-05-cintel-basic/",
+        target="_blank",
+    )
+
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
 
 
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+    
+ui.h2("Current Temperature")
+
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("warmer than usual")
+icon_svg("sun")
 
 
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
+
+
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Current Data (placeholder only)")
+
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Current Chart (placeholder only)")
